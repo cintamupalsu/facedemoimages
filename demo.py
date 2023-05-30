83d76028f3c7713ba45633832e146cf5c1b44b80
アリフ　マウラナ <a_maulana@sbs-infosys.co.jp>
2023-05-30 15:37:39 +0900

    u1追加

---
+++ demo.py
@@ -0,0 +1,4 @@
+import cv2
+import face_recognition as FR
+font=cv2.FONT_HERSEY_SIMPLEX
+makinokao=FR.load_image_file('/Users/s118301/Projects/2023/FaceRecognizer/benkyoukai/demoimages/known/makino_katsuhiko.jpg')
